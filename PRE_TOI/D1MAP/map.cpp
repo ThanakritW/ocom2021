f68222a62ec4813a1d9d8067a61cc15f400295f3
Thanakrit Wong-asa <gainthanakrit@gmail.com>
2021-11-07 23:04:34 +0700

    PreTOI

---
+++ PRE_TOI/D1MAP/map.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,k,tx,ty,p[40010],px,py,mp[410][410],lv[40010],v[40010][5],sx=500,sy=500;
+char c;
+bool f[40010];
+int dx[4]={-1,0,1,0};
+int dy[4]={0,1,0,-1};
+void construct(int s,int x,int y){
+    mp[x][y]=s;
+    f[s]=true;
+    if(x<sx) sx=x;
+    if(y<sy) sy=y;
+    for(int i=0;i<4;i++){
+        if(!f[v[s][i]]){
+            construct(v[s][i],x+dx[i],y+dy[i]);
+        }
+    }
+}
+int main(){
+    cin >> n >> m;
+    k=n*m;
+    for(int i=0;i<k;i++){
+        p[i]=i;
+        f[i]=false;
+    }
+    for(int i=0;i<k-1;i++){
+        cin >> tx >> c >> ty;
+        if(c=='U'){
+            v[tx][2]=ty;
+            v[ty][0]=tx;
+        }
+        else{
+            v[tx][1]=ty;
+            v[ty][3]=tx;
+        }
+    }
+    construct(0,205,205);
+    for(int i=sx;i<sx+n;i++){
+        for(int j=sy;j<sy+m;j++){
+            cout << mp[i][j] << " ";
+        }
+        cout << '\n';
+    }
+}
---
+++ Programminginth/L-sequence/tempCodeRunnerFile.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,k,km,a[1010],mn;
+bool f[1010];
+int main(){
+    cin >> n >> k;
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+        f[i]=1;
+    }
+    for(int i=0;i<n;i++){
+        mn=a[i];
+        km=0;
+        for(int j=0;j<=k;j++){
+            if(mn>a[i+j]){
+                mn=a[i+j];
+                km=j;
+            }
+        }
+        for(int j=0;j<km;j++){
+            f[i+j]=0;
+        }
+        i+=km;
+        k-=km;
+        if(k==0) break;
+    }
+    for(int i=0;i<n;i++){
+        if(f[i]){
+            cout << a[i] << " ";
+        }
+    }
+    return 0;
+}
